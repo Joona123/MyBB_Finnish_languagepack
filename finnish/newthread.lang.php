042d963bbd303fe1eca14c27187479ec8a5849d6
Joona <joona@engineer.com>
2016-11-06 20:07:58 +0200

    newthread

---
+++ finnish/newthread.lang.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['nav_newthread'] = "Aloita keskustelu";
+$l['newthread_in'] = "Aloita keskustelu alueelle {1}";
+$l['post_new_thread'] = "Aloita uusi ketju";
+$l['thread_subject'] = "Otsikko";
+$l['your_message'] = "Viesti:";
+$l['post_options'] = "Asetukset:";
+$l['options_sig'] = "<strong>Sisällytä allekirjoitus</strong> (vain rekisteröityneille käyttäjille)";
+$l['options_emailnotify'] = "<strong>Tilaa sähköpostiin</strong> ilmoitukset uusista vastauksista (vain rekisteröityneille käyttäjille)";
+$l['options_disablesmilies'] = "<strong>Poista käytöstä hymiöt</strong>";
+$l['post_thread'] = "Aloita ketju";
+$l['preview_post'] = "Esikatselu";
+$l['poll'] = "Äänestys:";
+$l['poll_desc'] = "Voit myös lisätä äänestyksen.";
+$l['poll_check'] = "Lisää äänestys";
+$l['num_options'] = "Vaihtoehtojen määrä:";
+$l['max_options'] = "(korkeintaan {1})";
+$l['mod_options'] = "Valvojan työkalut:";
+$l['close_thread'] = "<strong>Lukitse ketju</strong>: ketjuun ei voi vastata (sopii vaikkapa silloin, kun ketjun tarkoituksena on vain ilmoittaa jostakin asiasta).";
+$l['stick_thread'] = "<strong>Kiinnitä keskustelu:</strong> ketju näytetään ennen kiinnittämättömiä ketjuja.";
+$l['draft_saved'] = "Ketju on tallennettu luonnokseksi.<br />Sinut ohjataan luonnoksiin.";
+$l['image_verification'] = "Kuvavarmistus";
+$l['verification_note'] = "Kirjoita kuvassa näkyvät merkit. Tällä toiminnolla ehkäistään automaattista roskapostitusta.";
+$l['verification_subnote'] = "(kirjainkoolla ON väliä)";
+$l['invalid_captcha'] = "Syöttämäsi kuvavarmistus ei täsmännyt kuvassa näkyvän koodin kanssa. Kirjoita koodi niin kuin se näkyy kuvassa.";
+$l['error_post_already_submitted'] = "Olet jo lähettänyt tämän ketjun tälle alueelle. Näet ketjun vierailemalla alueella.";
+$l['no_prefix'] = "Ei etuliitettä";
+$l['forum_rules'] = "{1} - säännöt";
+$l['multiquote_external_one'] = "Olet valinnut lainauksen jostakin toisesta kejusta.";
+$l['multiquote_external'] = "Olet valinnut {1} ulkopuolista ketjua.";
+$l['multiquote_external_one_deselect'] = "poista valinta tästä viestistä";
+$l['multiquote_external_deselect'] = "poista valinta näistä viesteistä";
+$l['multiquote_external_one_quote'] = "Lainaa myös tämä viesti";
+$l['multiquote_external_quote'] = "Lainaa myös tätä viestiä";
+$l['redirect_newthread'] = "Kiitos, ketju on nyt aloitettu.";
+$l['redirect_newthread_poll'] = "<br />Siirryt luotavan äänestyksen asetuksiin.";
+$l['redirect_newthread_moderation'] = "<br />Ylläpito on määrännyt, että kaikki uudet ketjut vaativat hyväksynnän valvojalta. Sinut ohjataan takaisin ketjulistaan.";
+$l['redirect_newthread_thread'] = "<br />Siirryt aloitettuun ketjuun.";
+$l['invalidthread'] = "Määriteltyä luonnosta ei ole olemassa tai sinulla ei ole tarvittavia oikeuksia.";
+?>
