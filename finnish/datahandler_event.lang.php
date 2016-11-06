41b316ec5b24afd82bbf24f55bf9d2b0156828fa
Joona <joona@engineer.com>
2016-11-06 17:12:02 +0200

    datahandler_event

---
+++ finnish/datahandler_event.lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * MyBB Finnish languagepack * Suomentanut Joona
+ */
+
+$l['eventdata_missing_name'] = 'Tapahtuman nimi puuttuu. Palaa takaisin ja anna tapahtumalle nimi jatkaaksesi.';
+$l['eventdata_missing_description'] = 'Unohdit lisätä kuvauksen. Palaa takaisin ja lisää kuvaus jatkaaksesi.';
+
+$l['eventdata_invalid_start_date'] = 'Määrittelemäsi tapahtuman aloituspäivämäärä on virheellinen. Varmistathan että lisäsit päivän, kuukauden ja vuoden ja että valitsemasi päivä on olemassa.';
+$l['eventdata_invalid_start_year'] = "Tapahtumia voi luoda vain seuraavan viiden vuoden ajanjaksolle. Valitse kelvollinen aloitusvuosi listasta.";
+$l['eventdata_invalid_start_month'] = 'Lisäämäsi aloituskuukausi on virheellinen. Ole hyvä ja lisää kelvollinen aloituskuukausi.';
+
+$l['eventdata_invalid_end_date'] = 'Tapahtumasi päättymispäivämäärä ei kelpaa. Tarkista, että olet lisännyt päivän, kuukauden ja vuoden ja että valitsemasi päivä on olemassa.';
+$l['eventdata_invalid_end_year'] = "Tapahtumia voi luoda vain seuraavan viiden vuoden ajanjaksolle. Valitse kelvollinen päättymisvuosi jatkaaksesi.";
+$l['eventdata_invalid_end_month'] = 'Valitsemasi päättymiskuukausi ei kelpaa. Valitse kelvollinen kuukausi jatkaaksesi.';
+$l['eventdata_invalid_end_day'] = 'Päättymispäivä, jonka valitsit, ei ole kelvollinen. Valitsethan olemassaolevan päivän jatkaaksesi.';
+
+$l['eventdata_cant_specify_one_time'] = "Sinun täytyy merkitä sekä alkamis- että päättymisajankohta, ei riitä jos lisäät vain toisen.";
+$l['eventdata_start_time_invalid'] = "Valitsemasi alkamisaika on virheellinen. Kelvollisia muotoja ovat: 0:01, 00:01 ja 12:01am.";
+$l['eventdata_end_time_invalid'] = "Valitsemasi päättymisaika on virheellinen. Kelvollisia muotoja ovat: 0:01, 00:01 ja 12:01am.";
+$l['eventdata_invalid_timezone'] = "Tapahtumasi aikavyöhyke ei ole kelvollinen";
+$l['eventdata_end_in_past'] = "Lisäämäsi tapahtuman alkamis- ja päättymisajankohta eivät näytä olevan oikeassa järjestyksessä.";
+
+$l['eventdata_only_ranged_events_repeat'] = "Ainoastaan aikarajatut tapahtumat (joilla on alkamis- ja päättymisajankohta) voi toistaa.";
+$l['eventdata_invalid_repeat_day_interval'] = "Valitsemasi toistoväli ei ole kelvollinen.";
+$l['eventdata_invalid_repeat_week_interval'] = "Valitsemasi viikkojen toistoväli ei kelpaa.";
+$l['eventdata_invalid_repeat_weekly_days'] = "Unohdit valita viikonpäivän, jolloin tapahtuman olisi tarkoitus toistua.";
+$l['eventdata_invalid_repeat_month_interval'] = "valitsemasi kuukausien toistoväli ei kelpaa";
+$l['eventdata_invalid_repeat_year_interval'] = "Tuo vuosien toistoväli ei ole kelvollinen.";
+$l['eventdata_event_wont_occur'] = "Määrittelemilläsi toistoasetuksilla sekä aloitus- ja päättymisajoilla tämä tapahtuma ei tule koskaan esiintymään.";
+
+$l['eventdata_no_permission_private_event'] = "Sinulla ei ole tarvittavia oikeuksia yksityisen tapahtuman lisäämiseen.";
+?>
