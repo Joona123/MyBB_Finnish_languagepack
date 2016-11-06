a66a73ed0db16fd7e1d91a794ef2571e60b63f1e
Joona <joona@engineer.com>
2016-11-06 18:36:58 +0200

    misc

---
+++ finnish/misc.lang.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['nav_helpdocs'] = "Ohjeet";
+$l['nav_smilies'] = "Hymiöt";
+$l['nav_syndication'] = "Tuoreimmat ketjut (RSS)";
+
+$l['aol_im'] = "AOL-pikaviestin";
+$l['msn'] = "MSN Messenger";
+$l['yahoo_im'] = "Yahoo-pikaviestin";
+$l['msn_messenger_center'] = "MSN Messenger -keskus";
+$l['msn_address_is'] = "Käyttäjän {1} MSN on:";
+$l['yahoo_center'] = "Yahoo!-keskus";
+$l['send_y_message'] = "Lähetä Yahoo!-viesti käyttäjälle {1}.";
+$l['view_y_profile'] = "Näytä käyttäjän {1} Yahoo!-profiili.";
+$l['aim_center'] = "AOL-pikaviestikeskus";
+
+$l['download_aim'] = "Lataa AIM";
+$l['aim_remote'] = "AIM Remote";
+$l['send_me_instant'] = "Lähetä minulle pikaviesti";
+$l['add_me_buddy_list'] = "Lisää minut ystävälistallesi";
+$l['add_remote_to_page'] = "Lisää Remote sivullesi";
+$l['download_aol_im'] = "Lataa AOL-pikaviestin";
+
+$l['buddy_list'] = "Ystävät";
+$l['online'] = "Paikalla";
+$l['offline'] = "Poissa";
+$l['delete_buddy'] = "X";
+$l['pm_buddy'] = "Lähetä yksityisviesti";
+$l['last_active'] = "<strong>Oli aktiivinen:</strong> {1}";
+$l['close'] = "Sulje";
+$l['no_buddies'] = "<em>Ystävälistasi on tyhjä.</em>";
+
+$l['help_docs'] = "Ohjeet";
+
+$l['smilies_listing'] = "Hymiöt";
+$l['name'] = "Nimi";
+$l['abbreviation'] = "Lyhenne";
+$l['click_to_add'] = "Klikkaa hymiötä lisätäksesi sen viestiisi";
+$l['close_window'] = "sulje ikkuna";
+
+$l['who_posted'] = "Kirjoittaneet";
+$l['total_posts'] = "Viestejä yhteensä:";
+$l['user'] = "Tunnus";
+$l['num_posts'] = "# viestiä";
+
+$l['forum_rules'] = "{1} – Säännöt";
+
+$l['error_invalid_limit'] = "Antamasi syötteen kohtien rajoitus ei ole kelvollinen. Syötä kelvollinen rajoitus.";
+
+$l['syndication'] = "Tuoreimmat ketjut";
+$l['syndication_generated_url'] = "Luodun RSS-syötteen osoite:";
+$l['syndication_note'] = "Ohessa voit luoda haluamasilaisen RSS-syötteen. Linkkejä voi luoda kaikista alueista, tietystä alueesta tai useista alueista. Saat valinnat tehtyäsi linkin, jonka voit kopioida RSS-lukijaasi kuten <a href=\"http://www.sharpreader.net/\">SharpReader</a>. <i><a href=\"http://fi.wikipedia.org/wiki/RSS\">Mikä on RSS?</a></i>";
+$l['syndication_forum'] = "Seurattava alue:";
+$l['syndication_forum_desc'] = "Valitse alue. Voit valita CTRL-näppäimen avulla useampia huoneita.";
+$l['syndication_version'] = "Syötteen versio:";
+$l['syndication_version_desc'] = "Valitse versio, jonka mukaisesti syöte luodaan.";
+$l['syndication_version_atom1'] = "Atom 1.0";
+$l['syndication_version_rss2'] = "RSS 2.00 (oletus)";
+$l['syndication_generate'] = "Luo syötteen osoite";
+$l['syndication_limit'] = "Rajoitus:";
+$l['syndication_limit_desc'] = "Kerralla ladattavien ketjujen määrä. Sen tulisi olla alle 50.";
+$l['syndication_threads_time'] = "ketjua kerralla";
+$l['syndicate_all_forums'] = "Syöte kaikista alueista";
+
+$l['redirect_markforumread'] = "Valitut alueet on merkitty luetuiksi.";
+$l['redirect_markforumsread'] = "Kaikki alueet on merkitty luetuiksi.";
+$l['redirect_forumpasscleared'] = "Alueen talletettu salasana on nyt poistettu.";
+$l['redirect_cookiescleared'] = "Kaikki evästeet on poistettu.";
+
+$l['error_invalidimtype'] = "Käyttäjällä ei ole tätä pikaviestintä määritettynä profiilissaan.";
+$l['error_invalidhelpdoc'] = "Määritettyä ohjeistusartikkelia ei vaikuta olevan olemassa.";
+$l['error_invalidkey'] = "Sinua ei voitu varmentaa evästeiden poistoa varten. Tämän on saattanut aiheuttaa haitallinen JavaScript-koodi, joka pyrkii poistamaan evästeesi automaattisesti. Jos tavoitteenasi on poistaa evästeesi, voit tehdä sen evästeitä käsittelevän ohjesivujen kautta.";
+
+$l['dst_settings_updated'] = "Kesäajan tila on päivitetty automaattisesti.<br /><br />Sinua ohjataan etusivulle...";
+?>
