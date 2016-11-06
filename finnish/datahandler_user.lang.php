0c516fe1bbd8b50e1f159156ca16bbbf3e3a39f8
Joona <joona@engineer.com>
2016-11-06 17:14:34 +0200

    datahandler_user

---
+++ finnish/datahandler_user.lang.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * MyBB Finnish languagepack * Suomentanut Joona
+ */
+
+$l['userdata_missing_username'] = "Tunnus puuttuu.";
+$l['userdata_banned_username'] = "Tunnusta ei voi rekisteröidä.";
+$l['userdata_bad_characters_username'] = "Tunnus sisältää kiellettyjä merkkejä.";
+$l['userdata_invalid_username_length'] = "Tunnuksessa on oltava {1} – {2} merkkiä.";
+$l['userdata_invalid_usertitle_length'] = "Määrittelemäsi titteli on liian pitkä, se saa olla korkeintaan {1} merkkiä pitkä.";
+$l['userdata_username_exists'] = "Tunnus on varattu.";
+$l['userdata_invalid_password_length'] = "Salasanan kuuluu olla {1} – {2} merkkiä pitkä.";
+$l['userdata_no_complex_characters'] = "Salasanasi on liian helppo arvata. Hyvä salasana sisältää {1} merkkiä, vähintään yhden numeron sekä erikokoisia kirjaimia.";
+$l['userdata_passwords_dont_match'] = "Salasanat eivät täsmää.";
+$l['userdata_missing_email'] = "Sähköpostiosoite puuttuu.";
+$l['userdata_invalid_email_format'] = "Sähköpostiosoitteen muoto ei kelpaa.";
+$l['userdata_emails_dont_match'] = "Sähköpostiosoitteet eivät täsmää.";
+$l['userdata_banned_email'] = "Valitsemasi sähköpostiosoite on kiellettyjen listalla.";
+$l['userdata_email_already_in_use'] = "Syöttämäsi sähköpostiosoite on jo toisen tunnuksen käytössä.";
+$l['userdata_dob_required'] = "Syntymäajan syöttäminen on välttämätöntä. Voit piilottaa syntymäaikasi profiilistasi.";
+$l['userdata_invalid_website'] = "Sivuston osoite ei kelpaa.";
+$l['userdata_invalid_icq_number'] = "ICQ-numero ei ole kelvollinen.";
+$l['userdata_invalid_msn_address'] = "Kelvoton MSN-osoite.";
+$l['userdata_invalid_birthday'] = "Syöttämäsi syntymäaika ei kelpaa.";
+$l['userdata_invalid_birthday_coppa'] = "Valitse syntymävuotesi. Voit piilottaa sen profiilistasi.";
+$l['userdata_invalid_birthday_coppa2'] = "Sinun täytyy olla 13-vuotias tai vanhempi, mikäli haluat liittyä tälle sivustolle. Ota yhteyttä ylläpitäjään.";
+$l['userdata_invalid_birthday_privacy'] = 'Valitse syntymäpäivän yksityisyysasetus.';
+$l['userdata_invalid_referrer'] = "Emme tunnista tätä suosittelijaa. Syötä kelvollinen suosittelija tai jätä kenttä täyttämättä.";
+$l['userdata_invalid_language'] = "Valitsemaasi kieltä ei tunnisteta. Valitse kelvollinen kieli.";
+$l['userdata_missing_returndate'] = "Valitse täydellinen paluupäivämäärä. Muistathan täyttää kaikki kentät (päivä, kuukausi, vuosi).";
+$l['userdata_missing_required_profile_field'] = "Vaadittu kenttä jäi täyttämättä. Täytä kenttä tai valitse arvo.";
+$l['userdata_bad_profile_field_values'] = "Lisäsit kelvottoman arvon kenttään \"{1}\". Valitse arvo annetuista vaihtoehdoista.";
+$l['userdata_max_limit_reached'] = "Lisäsit liian pitkän arvon kenttään \"{1}\". Se voi olla korkeintaan {2} merkkiä pitkä.";
+$l['userdata_invalid_checkfield'] = "Olemme tunnistaneet sinut botiksi. Jos tämä ei ole totta, voit tehdä asiasta valituksen ylläpidolle.";
+$l['userdata_invalid_postnum'] = "Virheellinen viestimäärä. Syötä kelvollinen määrä tai jätä kohta tyhjäksi.";
+?>
