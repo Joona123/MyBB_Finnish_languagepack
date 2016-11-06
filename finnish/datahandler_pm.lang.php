0f64af400f10f20fadb24aa195b31cbf7eae1c74
Joona <joona@engineer.com>
2016-11-06 17:13:05 +0200

    datahandler_pm

---
+++ finnish/datahandler_pm.lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona */
+
+$l['pmdata_too_long_subject'] = "Kirjoittamasi otsikko on liian pitkä.";
+$l['pmdata_code_no_subject'] = "[ei otsikkoa]";
+$l['pmdata_missing_subject'] = "Otsikko puuttuu.";
+$l['pmdata_missing_message'] = "Viestistä puuttuu sisältö.";
+$l['pmdata_invalid_recipients'] = "Nämä vastaanottajat eivät ole kelvollisia: {1}";
+$l['pmdata_no_recipients'] = "'Vastaanottaja' -kentästä puuttuvat vastaanottajat.";
+$l['pmdata_too_many_recipients'] = "Viestin voi lähettää kerrallaan korkeintaan {1} eri käyttäjälle.";
+$l['pmdata_recipient_is_ignoring'] = "Viestin lähettäminen ei onnistunut, koska viestin vastaanottaja ({1}) on estänyt sinut.";
+$l['pmdata_recipient_has_buddy_only'] = "Viestin lähettäminen käyttäjälle {1} epäonnistui, sillä hän on sallinut viestit vain ystäviltään.";
+$l['pmdata_recipient_pms_disabled'] = 'Käyttäjä {1} on poistanut yksityisviestit käytöstä, joten viestiä ei voitu lähettää.';
+$l['pmdata_recipient_reached_quota'] = 'Käyttäjän {1} postilaatikko on täysi, joten viestiä ei pystytty lähettämään.';
+$l['pmdata_pm_flooding'] = 'Lähetät viestejä liian nopeasti. Nyt sinun on odotettava {1} sekuntia ennen kuin voit lähettää seuraavan viestisi.';
+$l['pmdata_pm_flooding_one_second'] = 'Sinun täytyy odottaa vielä sekunti. Sitten voit lähettää uuden viestin.';
+?>
