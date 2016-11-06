a49a0f4083f325d453a883bc19e8f108204fc191
Joona <joona@engineer.com>
2016-11-06 21:38:46 +0200

    syndication

---
+++ finnish/syndication.lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['all_forums'] = "Kaikki alueet";
+$l['forum'] = "Alue:";
+$l['posted_by'] = "Kirjoittanut:";
+$l['on'] = "";
+?>
