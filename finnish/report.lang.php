e16ea9a356173f12bcceb7ab7929ca9912b51dc6
Joona <joona@engineer.com>
2016-11-06 20:56:08 +0200

    report

---
+++ finnish/report.lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['report_post'] = "Ilmianna viesti";
+$l['report_to_mod'] = "Ilmianna valvojalle";
+$l['only_report'] = "Ainoastaan roskaposti, luvaton mainostus ja palvelun väärinkäyttö kannattaa ilmiantaa.";
+$l['report_reason'] = "Kerro miksi ilmiannat tämän:";
+$l['thank_you'] = "Kiitos.";
+$l['post_reported'] = "Viesti on onnistuneesti ilmiannettu. Nyt voit sulkea ikkunan.";
+$l['report_error'] = "Virhe ilmiantamisessa";
+$l['no_reason'] = "Unohdit kertoa miksi ilmiannoit.";
+$l['go_back'] = "Takaisin";
+$l['close_window'] = "Sulje ikkuna";
+?>
