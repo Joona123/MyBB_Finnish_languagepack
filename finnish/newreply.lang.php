5f3579c35b59204beb5c1b6c1b41b8059ab238a6
Joona <joona@engineer.com>
2016-11-06 19:56:51 +0200

    newreply

---
+++ finnish/newreply.lang.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['nav_newreply'] = "Vastauksen kirjoittaminen";
+$l['post_reply_to'] = "Vastaus keskusteluun {1}";
+$l['post_new_reply'] = "Lähetä vastaus";
+$l['reply_to'] = "Vastaus ketjuun {1}";
+$l['post_subject'] = "Viestin otsikko:";
+$l['your_message'] = "Viesti:";
+$l['post_options'] = "Asetukset:";
+$l['options_sig'] = "<strong>Sisällytä allekirjoitus</strong> (Rekisteröityneille käyttäjille.++)";
+$l['options_emailnotify'] = "<strong>Tilaa sähköpostiin</strong> ilmoitukset uusista vastauksista (vain rekisteröityneille käyttäjille)";
+$l['options_disablesmilies'] = "<strong>Hymiöt pois käytöstä</strong>";
+$l['post_reply'] = "Lähetä vastaus";
+$l['preview_post'] = "Esikatselu";
+$l['mod_options'] = "Valvojan työkalut:";
+$l['close_thread'] = "<strong>Lukitse ketju</strong>: ketjuun ei voi enää kirjoittaa vastauksia.";
+$l['stick_thread'] = "<strong>Kiinnitä keskustelu:</strong> ketju näytetään ennen kiinnittämättömiä ketjuja.";
+$l['forum_rules'] = "{1} - säännöt";
+$l['thread_review'] = "Ketju (uusimmat viestit ensin)";
+$l['thread_review_more'] = "Ketjussa on enemmän kuin {1} vastausta. <a href=\"{2}\">Avaa koko keskustelu</a>.";
+$l['posted_by'] = "Kirjoittaja";
+$l['draft_saved'] = "Viesti on tallennettu luonnoksiin.<br />Sinut ohjataan  luonnoksiin.";
+$l['image_verification'] = "Kuvavarmistus";
+$l['verification_note'] = "Kirjoita kuvassa näkyvät merkit. Tällä toiminnolla ehkäistään automaattista roskapostitusta.";
+$l['verification_subnote'] = "(kirjainkoolla ON väliä)";
+$l['invalid_captcha'] = "Syöttämäsi kuvavarmistus ei täsmännyt kuvassa näkyvän koodin kanssa. Kirjoita koodi niin kuin se näkyy kuvassa.";
+$l['error_post_already_submitted'] = "Olet jo vastannut tähän ketjuun. Vieraile keskustelussa nähdäksesi viestisi.";
+$l['multiquote_external_one'] = "Olet valinnut lainauksen jostakin toisesta keskustelusta.";
+$l['multiquote_external'] = "Olet valinnut {1} ulkopuolista keskustelua.";
+$l['multiquote_external_one_deselect'] = "poista valinta tästä viestistä";
+$l['multiquote_external_deselect'] = "poista valinta näistä viesteistä";
+$l['multiquote_external_one_quote'] = "Lainaa myös tämä viesti";
+$l['multiquote_external_quote'] = "Lainaa myös tätä viestiä";
+$l['redirect_newreply'] = "Kiitos viestistäsi, se on lisätty ketjun jatkoksi.";
+$l['redirect_newreply_moderation'] = "Ylläpito on määrännyt, että kaikki uudet viestit vaativat hyväksynnän valvojalta. Sinut ohjataan takaisin ketjuun.";
+$l['redirect_newreply_post'] = "<br />Sinut ohjataan viestiisi.";
+$l['redirect_newreplyerror'] = "Viestisi evättiin, koska siinä ei ollut tarpeeksi sisältöä. <br />Sinut ohjataan takaisin ketjuun.";
+$l['redirect_threadclosed'] = "Ketju on lukittu, joten siihen ei enää voi vastata.";
+$l['error_post_noperms'] = "Oikeutesi eivät riitä tämän luonnoksen muokkaamiseen.";
+?>
