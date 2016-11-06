c4f2dac8a16c046aa667972aff028d0be2e0d453
Joona <joona@engineer.com>
2016-11-06 21:25:55 +0200

    sendthread

---
+++ finnish/sendthread.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['nav_sendthread'] = "Lähetä keskustelu ystävälle";
+$l['send_thread'] = "Lähetä ystävälle";
+$l['recipient'] = "Vastaanottaja:";
+$l['recipient_note'] = "Kirjoita ystäväsi sähköpostiosoite";
+$l['subject'] = "Otsikko:";
+$l['message'] = "Viesti:";
+$l['error_nosubject'] = "Kirjoita otsikko, jotta viesti voidaan lähettää";
+$l['error_nomessage'] = "Kirjoita viesti, jotta sähköposti voidaan lähettää";
+?>
