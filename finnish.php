7cf169a986fc6f2d088d7bd96cc6d476435ecc17
Joona <joona@engineer.com>
2016-11-06 20:25:28 +0200

    finnish.php

---
+++ finnish.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ *
+ * Suomentanut Joona
+ *
+ * Suomennos päivitetty 6.11.2016
+ *
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "suomi (Finnish)";
+
+// The author of the language
+$langinfo['author'] = "joona@engineer.com";
+
+// The language authors website
+$langinfo['website'] = "http://github.com/joona123/MyBB_Finnish_languagepack";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1611";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 0;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "fi";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
+?>
