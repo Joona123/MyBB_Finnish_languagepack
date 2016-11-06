821c5f76a86f99503eb2618f947d27c2094752fb
Joona <joona@engineer.com>
2016-11-06 17:04:47 +0200

    akismet

---
+++ finnish/akismet.lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['spam'] = "Roskapostia";
+
+$l['post_spam_success'] = "Viesti(t) on onnistuneesti merkitty roskapostiksi.";
+$l['thread_spam_success'] = "Keskusteluketju on merkitty roskapostiksi.";
+
+$l['mark_as_spam'] = "Merkitse roskapostiksi";
+$l['confirm_mark_as_spam'] = "Haluatko varmasti merkitä valitun viestin tai ketjun roskapostiksi?";
+
+$l['akismet_error'] = "Akismet-virhe";
+
+$l['redirect_newthread'] = "Akismet on tunnistanut viestisi roskapostiksi ja sitä ei käsitellä pidemmälle.<br />Mikäli tämä ei ole totta, ota yhteyttä keskustelualueen ylläpitoon.";
+$l['redirect_return_forum'] = "Voit siirtyä <a href=\"{1}\">takaisin alueelle</a>.";
+
+$l['redirect_newreply'] = "Akismet on tunnistanut vastauksesi roskapostiksi ja sitä ei käsitellä pidemmälle.<br />Mikäli tämä ei ole totta, ota yhteyttä keskustelualueen ylläpitoon.";
+$l['redirect_return_thread']= "Voit siirtyä <a href=\"{1}\">takaisin ketjuun</a><br />tai vaihtoehtoisesti <a href=\"{2}\">takaisin keskustelualueen etusivulle</a>.";
+
+?>
