3c101d1cbdd6bd1d86a4a9bf24dddb64c50876a2
Joona <joona@engineer.com>
2016-11-06 20:52:37 +0200

    ratethread

---
+++ finnish/ratethread.lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['redirect_threadrated'] = "Kiitos, että arvostelit ketjun. Sinut ohjataan takaisin ketjuun.";
+$l['error_invalidrating'] = "Antamasi arvostelu ei ole kelvollinen tälle ketjulle. Siirry takaisin ja yritä uudelleen.";
+$l['error_alreadyratedthread'] = "Olet jo arvostellut tämän ketjun.";
+$l['rating_votes_average'] = "{1} ääntä – keskiarvo {2}/5";
+$l['one_star'] = "1 / 5";
+$l['two_stars'] = "2 / 5";
+$l['three_stars'] = "3 / 5";
+$l['four_stars'] = "4 / 5";
+$l['five_stars'] = "5 / 5";
+$l['rating_added'] = "Arvostelusi on huomioitu!";
+?>
