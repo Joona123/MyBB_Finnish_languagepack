6d4bf8848c09ccaf77374f177d070d501e65f245
Joona <joona@engineer.com>
2016-11-06 17:06:51 +0200

    announcements

---
+++ finnish/announcements.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['nav_announcements'] = "Ilmoitus";
+$l['announcements'] = "Ilmoitus";
+$l['forum_announcement'] = "Keskustelualueen ilmoitus: {1}";
+$l['error_invalidannouncement'] = "Ilmoitus ei ole kelvollinen.";
+
+$l['announcement_edit'] = "Muokkaa ilmoitusta";
+$l['announcement_qdelete'] = "Poista ilmoitus";
+$l['announcement_quickdelete_confirm'] = "Haluatko varmasti poistaa ilmoituksen?";
+
+?>
