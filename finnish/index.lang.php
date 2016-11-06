c49ad6bf118e04481302f91af8b4f80f49d6ff2d
Joona <joona@engineer.com>
2016-11-06 17:22:37 +0200

    index

---
+++ finnish/index.lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona */
+
+$l['boardstats'] = "Tilastot";
+$l['new_posts'] = "Alueella on uusia viestejä";
+$l['no_new_posts'] = "Alueella ei ole uusia viestejä";
+$l['forum_locked'] = "Alue on lukittu";
+$l['forum_unapproved_posts_count'] = "Alueella on {1} hyväksyntää odottavaa viestiä.";
+$l['forum_unapproved_post_count'] = "Alueella on hyväksyntää odottava viesti.";
+$l['forum_unapproved_threads_count'] = "Alueella on {1} hyväksyntää odottavaa ketjua.";
+$l['forum_unapproved_thread_count'] = "Alueella on hyväksyntää odottava ketju.";
+$l['markread'] = "Merkitse kaikki viestit luetuiksi";
+$l['forumteam'] = "Tiimi";
+$l['forumstats'] = "Tilastot";
+$l['todays_birthdays'] = "Syntymäpäivät tänään";
+$l['birthdayhidden'] = "Piilotettu";
+$l['quick_login'] = "Pikakirjautuminen:";
+$l['index_logout'] = "Kirjaudu ulos";
+$l['private_messages'] = "Yksityisviestit";
+$l['pms_new'] = "Sinulle on {1} uutta yksityisviestiä.";
+$l['pms_unread_total'] = "(Sinulle on {1} lukematonta viestiä ja yhteensä {2} viestiä.)";
+$l['stats_posts_threads'] = "Yhteensä {1} viestiä ja {2} ketjua.";
+$l['stats_numusers'] = "Yhteensä {1} käyttäjää.";
+$l['stats_newestuser'] = "Uusin käyttäjämme on <b>{1}</b>";
+$l['stats_mostonline'] = "Parhaimmillaan {1} kävijää paikalla yhtä aikaa ({2} <abbr title="kello">klo</abbr> {3})";
+$l['whos_online'] = "Paikalla";
+$l['complete_list'] = "kaikki";
+$l['online_online_plural'] = "kävijää";
+$l['online_online_singular'] = "kävijä";
+$l['online_member_plural'] = "käyttäjää";
+$l['online_member_singular'] = "käyttäjä";
+$l['online_anon_plural'] = "";
+$l['online_anon_singular'] = "on";
+$l['online_guest_plural'] = "vierasta";
+$l['online_guest_singular'] = "vieras";
+$l['online_note'] = "{1} {2} paikalla kuluneen {3} minuutin sisällä ({4} {5}, {6} näkymätöntä, {8} {9}).";
+$l['subforums'] = "<strong>Sisäalueet:</strong>";
+$l['click_mark_read'] = "Merkitse luetuksi";
+?>
