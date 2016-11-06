1f54263158ab4ab717d980fe810e371f430aa0b8
Joona <joona@engineer.com>
2016-11-06 17:55:13 +0200

    memberlist

---
+++ finnish/memberlist.lang.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * MyBB Finnish languagepack * Suomentanut Joona */
+
+$l['nav_memberlist'] = "Käyttäjäluettelo";
+$l['memberlist_disabled'] = "Käyttäjäluettelo on poistettu käytöstä.";
+$l['member_list'] = "Käyttäjät";
+$l['avatar'] = "Avatar";
+$l['username'] = "Tunnus";
+$l['joined'] = "Liittynyt";
+$l['lastvisit'] = "Viimeisin vierailu";
+$l['posts'] = "Viestejä";
+$l['referrals'] = "Suositeltuja";
+$l['search_members'] = "Etsi käyttäjää";
+$l['website'] = "Sivusto";
+$l['sort_by'] = "Järjestä";
+$l['contains'] = "Sisältää:";
+$l['sort_by_username'] = "Järjestä tunnuksen perusteella";
+$l['sort_by_regdate'] = "Järjestä liittymisajankohdan perusteella";
+$l['sort_by_lastvisit'] = "Järjestä viimeisimmän vierailun perusteella";
+$l['sort_by_posts'] = "Järjestä viestien perusteella";
+$l['sort_by_referrals'] = "Järjestä suosittelujen perusteella";
+$l['order_asc'] = "nouseva järjestys";
+$l['order_desc'] = "laskeva järjestys";
+$l['forumteam'] = "Tiimi";
+
+$l['advanced_search'] = "Tarkennettu haku";
+$l['search_member_list'] = "Etsi käyttäjää";
+$l['search_criteria'] = "Hakusana";
+$l['begins_with'] = "alkaa";
+$l['username_contains'] = "sisältää";
+$l['search_website'] = "Sivusto";
+$l['search_aim'] = "AIM";
+$l['search_msn'] = "Windows Live Messenger";
+$l['search_yahoo'] = "Yahoo! Messenger";
+$l['search_icq'] = "ICQ-numero";
+$l['search_options'] = "Hakuasetukset";
+$l['per_page'] = "Tuloksia sivulla";
+$l['search'] = "Haku";
+
+$l['error_no_members'] = "<p>Käyttäjiä ei löytynyt.</p>";
+
+$l['a'] = 'A';
+$l['b'] = 'B';
+$l['c'] = 'C';
+$l['d'] = 'D';
+$l['e'] = 'E';
+$l['f'] = 'F';
+$l['g'] = 'G';
+$l['h'] = 'H';
+$l['i'] = 'I';
+$l['j'] = 'J';
+$l['k'] = 'K';
+$l['l'] = 'L';
+$l['m'] = 'M';
+$l['n'] = 'N';
+$l['o'] = 'O';
+$l['p'] = 'P';
+$l['q'] = 'Q';
+$l['r'] = 'R';
+$l['s'] = 'S';
+$l['t'] = 'T';
+$l['u'] = 'U';
+$l['v'] = 'V';
+$l['w'] = 'W';
+$l['x'] = 'X';
+$l['y'] = 'Y';
+$l['z'] = 'Z';
+$l['å'] = 'Å';
+$l['ä'] = 'Ä';
+$l['ö'] = 'Ö';
+
+?>
