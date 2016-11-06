e9da873e71a98dbfe7015394dda6af6df31be62f
Joona <joona@engineer.com>
2016-11-06 17:07:51 +0200

    archive

---
+++ finnish/archive.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona */
+
+$l['archive_fullversion'] = "Koko versio:";
+$l['archive_replies'] = "Vastaukset";
+$l['archive_reply'] = "Vastaa";
+$l['archive_pages'] = "Sivut:";
+$l['archive_note'] = "Selaat kevyttä arkistoversiota. Siirry <a href=\"{1}\">keskustelualueen kokoversioon</a>.";
+$l['archive_reference_urls'] = "Osoiteviittaus";
+$l['archive_nopermission'] = "Sinulla ei ole vaadittuja oikeuksia.";
+$l['error_nothreads'] = "Ei aiheita.";
+$l['error_unapproved_thread'] = "Tätä keskustelua ei olla hyväksytty. Tarkastele tämän keskustelun <a href=\"{1}\">kokoversiota</a> nähdäksesi sen sisällön.";
+$l['archive_not_found'] = "<h1>404 – Ei löytynyt</h1><p>Hakemaasi sivua ei löytynyt  palvelimeltamme.</p>";
+?>
