aac4e120ea79abfcbe718ec033dd4f6de7432266
Joona <joona@engineer.com>
2016-11-06 20:44:06 +0200

    printthread

---
+++ finnish/printthread.lang.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['forum'] = "Huone:";
+$l['printable_version'] = "Tulostettava versio";
+$l['pages'] = "Sivut:";
+$l['thread'] = "Keskustelu:";
+?>
