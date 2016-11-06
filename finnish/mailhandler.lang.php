799c421e3e27e049b3678fcd7b6ae30b81f28e12
Joona <joona@engineer.com>
2016-11-06 17:24:19 +0200

    mailhandler

---
+++ finnish/mailhandler.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+ 
+$l['error_no_connection'] = 'Virhe yhteyden muodostamisessa palvelimelle: ';
+$l['error_no_message'] = 'Viestiä ei määritetty.';
+$l['error_no_subject'] = 'Otsikkoa ei määritetty.';
+$l['error_no_recipient'] = 'Vastaanottajaa ei määritetty.';
+$l['error_not_sent'] = 'Tapahtui virhe käytettäessä PHP:n mail-funktiota.';
+$l['error_status_missmatch'] = 'Palvelimen antama tila ei vastaa odotettua tulosta: ';
+$l['error_data_not_sent'] = 'Tätä tietoa ei kyetty lähettämään palvelimelle: ';
+
+$l['error_occurred'] = 'Ainakin yksi virhe on tapahtunut. Korjaa mainitut virheet ennen jatkamista.<br />';
+?>
