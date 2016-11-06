45835472639dd391abcc729baeae69c2d1dec972
Joona <joona@engineer.com>
2016-11-06 17:13:50 +0200

    datahandler_post

---
+++ finnish/datahandler_post.lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * MyBB Finnish languagepack * Suomentanut Joona
+ */
+
+$l['postdata_invalid_user_id'] = "Hakemaasi käyttäjää ei vaikuta olevan olemassa. Tarkista, että syöttämäsi ID on oikein.";
+$l['postdata_firstpost_no_subject'] = "Unohdit kirjoittaa otsikon. Kirjoita viestillesi otsikko ja yritä uudelleen.";
+$l['postdata_missing_subject'] = "Viestistäsi puuttuu otsikko.";
+$l['postdata_missing_message'] = "Viestistäsi puuttuu sisältö.";
+$l['postdata_message_too_long'] = "Tämä viesti on liian pitkä. Viestin täytyy olla lyhyempi kuin {1} merkkiä (tällä hetkellä viestisi pituus on {2} merkkiä).";
+$l['postdata_message_too_short'] = "Viesti on liian lyhyt. Viestin pituuden tulee olla vähintään {1} merkkiä.";
+$l['postdata_subject_too_long'] = 'Kirjoittamasi otsikko on liian pitkä, otsikko saa olla korkeintaan 85 merkkiä pitkä (tällä hetkellä otsikkosi pituus on {1} merkkiä).';
+$l['postdata_post_flooding'] = "Lähetät viestejä liian nopeasti. Voit lähettää seuraavan viestisi {1} sekunnin kuluttua.";
+$l['postdata_post_flooding_one_second'] = "Voit lähettää seuraavan viestisi sekunnin kuluttua.";
+$l['postdata_too_many_images'] = "Viestissäsi on liian monta - tällä hetkellä {1} - kuvaa, mutta yhdessä viestissä saa olla korkeintaan {2} kuvaa.";
+$l['postdata_too_many_videos'] = 'Viestissäsi on liian monta videota: olet lisännyt {1} videota, vaikka viestissä saa olla korkeintaan {2} videota.';
+
+$l['thread_closed'] = "Tämä keskustelu on suljettu";
+$l['thread_opened'] = "Keskustelu on avattu";
+$l['thread_stuck'] = "Keskustelu on kiinnitetty";
+$l['thread_unstuck'] = "Keskustelun kiinnitys on poistettu";
+?>
