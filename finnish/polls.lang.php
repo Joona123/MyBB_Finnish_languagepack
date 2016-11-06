8277572b25032879c350599b0ddeb1702eeb104b
Joona <joona@engineer.com>
2016-11-06 20:36:45 +0200

    polls

---
+++ finnish/polls.lang.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['nav_postpoll'] = "Lisää äänestys";
+$l['nav_editpoll'] = "Muokkaa äänestystä";
+$l['nav_pollresults'] = "Äänestyksen tulokset";
+$l['edit_poll'] = "Muokkaa äänestystä";
+$l['delete_poll'] = "Poista äänestys";
+$l['delete_q'] = "Poista?";
+$l['delete_note'] = "Poistaakseksi äänestyksen ruksaa oheinen valinta ja paina sen vieressä olevaa painiketta.";
+$l['delete_note2'] = "<b>Huomaa:</b> kyselyä ei voi palauttaa.";
+$l['question'] = "Kysymys:";
+$l['num_options'] = "Vaihtoehtoja:";
+$l['max_options'] = "Korkeintaan:";
+$l['poll_options'] = "Äänestyksen vaihtoehdot:";
+$l['update_options'] = "Päivitä vaihtoehdot";
+$l['poll_options_note'] = "Vaihtoehtojen kuuluu olla lyhyitä ja ytimekkäitä.";
+$l['options'] = "Valinnat:";
+$l['option_multiple'] = "<b>Salli useampi valinta:</b> Käyttäjät voivat valita useamman kuin yhden vaihtoehdon.";
+$l['option_public'] = "<b>Julkinen äänestys:</b> Käyttäjät näkevät toistensa äänet.";
+$l['option_closed'] = "<b>Äänestys on suljettu:</b> Jos valitset tämän, äänestys on suljettu.";
+$l['poll_timeout'] = "Sulkeutuminen:";
+$l['timeout_note'] = "Äänestyksen kesto päivissä, jonka se on avoin.<br />(Arvolla 0 kysely pysyy auki ikuisesti.)";
+$l['days_after'] = "päivän jälkeen:";
+$l['update_poll'] = "Päivitä äänestys";
+$l['option'] = "Vaihtoehto";
+$l['votes'] = "Äänet:";
+$l['post_new_poll'] = "Lisää uusi äänestys";
+$l['days'] = "päivää";
+$l['poll_results'] = "Tulokset";
+$l['poll_total'] = "Yhteensä:";
+$l['poll_votes'] = "ääntä";
+$l['redirect_pollposted'] = "Äänestys on luotu.<br />Sinut ohjataan eteenpäin.";
+$l['redirect_pollpostedmoderated'] = "Äänestys on lisätty, mutta ketju odottaa valvojan hyväksyntää.<br />Sinut ohjataan takaisin alueelle.";
+$l['redirect_pollupdated'] = "Äänestys on päivitetty.<br />Sinut ohjataan takaisin ketjuun.";
+$l['redirect_votethanks'] = "Kiitos äänestäsi.<br />Sinut ohjataan takaisin ketjuun.";
+$l['redirect_unvoted'] = "Äänesi tässä ketjussa on poistettu.<br />Sinut ohjataan takaisin ketjuun.";
+$l['redirect_polldeleted'] = "Äänestys on poistettu ketjusta.<br />Sinut ohjataan takaisin ketjuun.";
+$l['error_polloptiontoolong'] = "Vähintään yksi antamistasi kyselyn vaihtoehdoista on sallittua rajaa pidempi. Siirry takaisin ja lyhennä niitä.";
+$l['error_noquestionoptions'] = "Et syöttänyt äänestykseen kysymystä tai vaihtoehtoja ei ole riittävästi. Vaihtoehtoja tulee olla vähintään 2.<br />Siirry takaisin ja korjaa asia.";
+$l['error_pollalready'] = "Ketjulla on jo äänestys!";
+$l['error_nopolloptions'] = "Määritelty äänestyksen vaihtoehto ei ole kelvollinen tai sitä ei ole olemassa.";
+$l['error_alreadyvoted'] = "Olet jo vastannut tähän äänestykseen.";
+$l['error_notvoted'] = "Et olr vastannut tähän äänestykseen.";
+$l['error_invalidpoll'] = "Määriteltyä äänestystä ei ole.";
+$l['error_pollclosed'] = "Et voi vastata äänestykseen, joka on suljettu.";
+$l['poll_deleted'] = "Poista äänestys";
+$l['poll_edited'] = "Muokkaa äänestystä";
+?>
