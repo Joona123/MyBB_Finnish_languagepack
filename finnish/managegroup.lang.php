bf52d406c24fe99aa7c75c11be4a349e73e5007a
Joona <joona@engineer.com>
2016-11-06 17:25:52 +0200

    managegroup

---
+++ finnish/managegroup.lang.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * MyBB Finnish languagepack * Suomentanut Joona
+ */
+
+$l['nav_group_management'] = "Ryhmän hallinta ({1})";
+$l['nav_join_requests'] = "Hakemukset";
+$l['nav_group_memberships'] = "Jäsenyydet ryhmissä";
+
+$l['not_leader_of_this_group'] = "Et ole tämän ryhmän vetäjä.";
+$l['invalid_group'] = "Pyytämääsi ryhmää ei ole.";
+$l['pending_requests'] ="Odottavat hakemukset";
+$l['num_requests_pending'] = "Ryhmään on tällä hetkellä <strong>{1}</strong> hakemusta.";
+$l['group_management'] = "Ryhmän hallinta";
+$l['members_of'] = "Jäsenet ({1})";
+$l['user_name'] = "Tunnus";
+$l['contact'] = "Yhteystiedot";
+$l['reg_date'] = "Rekisteröityi";
+$l['post_count'] = "Viestejä";
+$l['remove_selected'] = "Poista valitut käyttäjät ryhmästä";
+$l['add_member'] = "Lisää käyttäjä ({1})";
+$l['add_member_submit'] = "Lisää käyttäjä ryhmään";
+$l['join_requests'] = "Hakemukset";
+$l['join_requests_title'] = "Hakemukset ryhmään ({1})";
+$l['leader'] = "(vetäjä)";
+$l['reason'] ="Syy";
+$l['accept'] = "Hyväksy";
+$l['ignore'] = "Ohita";
+$l['decline'] = "Hylkää";
+$l['action_requests'] = "Suorita toimenpiteet";
+$l['join_requests_moderated'] = "Hakemukset on käsitelty.<br />Sinut ohjataan takaisin listaukseen.";
+$l['no_requests'] = "Ryhmällä ei ole tällä hetkellä hakemuksia käsiteltävänä.";
+$l['no_users'] = "Ryhmässä ei ole käyttäjiä.";
+$l['user_added'] = "Käyttäjä on lisätty ryhmään.";
+$l['users_removed'] = "Valitut käyttäjät on poistettu ryhmästä.";
+$l['group_no_members'] = "Tällä ryhmällä ei ole jäseniä.<br />Palaa<a href="usercp.php?action=usergroups"> ryhmien hallintaan</a>.";
+$l['group_public_moderated'] = "Tämä ryhmä on julkinen, joten kuka tahansa voi liittyä siihen. Ryhmän vetäjän tehtävä on silti tarkistaa hakemukset ennen hyväksymistä.";
+$l['group_public_not_moderated'] = "Tämä ryhmä on julkinen ryhmä, johon kuka tahansa saa liittyä.";
+$l['group_private'] = "Tämä on yksityinen ryhmä. Vain ryhmän vetäjä voi lisätä käyttäjiä ryhmään.";
+$l['group_default'] = "Tämä ryhmä on käyttäjien ydinryhmä.";
+$l['group_leaders'] = "Ryhmän vetäjät";
+$l['no_users_selected'] = "Ainuttakaan jäsentä ei valittu poistettavaksi...<br />Palaa takaisin ja valitse jäsenet jotka poistetaan ryhmästä.";
+
+$l['error_alreadyingroup'] = "Tämä käyttäjä on jo ryhmän jäsen.";
+?>
