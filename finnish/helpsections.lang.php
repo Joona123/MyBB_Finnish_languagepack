49e568adf7efda4115c0724e700b2f6082ebf5f9
Joona <joona@engineer.com>
2016-11-06 17:20:57 +0200

    helpsections

---
+++ finnish/helpsections.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona */
+
+/* 1 */
+
+// Help Section 1
+$l['s1_name'] = "Tunnuksen hallinta";
+$l['s1_desc'] = "Oman käyttäjätunnuksesi hallinta.";
+
+// Help Section 2
+$l['s2_name'] = "Viestien hallinta";
+$l['s2_desc'] = "Kirjoittaminen, vastaaminen jne.";
+?>
