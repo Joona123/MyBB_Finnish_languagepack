6457d1eacd09251deed7fc47d6a89a460953adf8
Joona <joona@engineer.com>
2016-11-06 17:16:34 +0200

    editpost

---
+++ finnish/editpost.lang.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['nav_editpost'] = "Muokkaa tätä viestiä";
+$l['edit_post'] = "Muokkaa tätä viestiä";
+$l['delete_post'] = "Poista tämä viesti";
+$l['delete_q'] = "Poista?";
+$l['delete_1'] = "Raksita ruutu ja klikkaa painiketta poistaaksesi.";
+$l['delete_2'] = "<b>Huomio:</b> jos tämä on ketjun ensimmäinen viesti, muutkin ketjun viestit poistetaan.";
+$l['subject'] = "Otsikko";
+$l['your_message'] = "Viesti";
+$l['post_options'] = "Valinnat:";
+$l['options_sig'] = "<strong>Allekirjoitus:</strong> sisällytä viestiin. (Rekisteröityneille käyttäjille.)";
+$l['options_emailnotify'] = "<strong>Sähköposti-ilmoitus:</strong> saat sähköpostiisi ilmoituksen uusista vastauksista tähän ketjuun. (Rekisteröityneille käyttäjille.)";
+$l['options_disablesmilies'] = "<strong>Hymiöt pois:</strong> poistaa tästä viestistä hymiöt käytöstä.";
+$l['preview_post'] = "Esikatselu";
+$l['update_post'] = "Päivitä";
+$l['poll'] = "Äänestys:";
+$l['poll_desc'] = "Jos haluat, voit sisällyttää viestiin äänestyksen";
+$l['poll_check'] = "Haluan lisätä äänestyksen";
+$l['num_options'] = "Vaihtoehtojen määrä:";
+$l['max_options'] = "(enintään {1})";
+$l['delete_now'] = "Poista nyt";
+$l['edit_time_limit'] = "Anteeksi, et voi muokata viestiäsi. Viestejä voi muokata {1} minuutin sisällä niiden lähettämisestä.";
+$l['no_prefix'] = "Ei etuliitettä";
+$l['redirect_nodelete'] = "Viestiä ei poistettu. Raksita laatikko ja koeta uudelleen.";
+$l['redirect_postedited'] = "Viestiä on muokattu.<br />";
+$l['redirect_postedited_redirect'] = "Sinut ohjataan piakkoin takaisin ketjuun.";
+$l['redirect_postedited_poll'] = "Viestiä on muokattu.<br />Koska valitsit haluavasi sisällyttää äänestyksen, sinut ohjataan pian sivulle, jolta käsin voit luoda äänestyksesi.";
+$l['error_invalidpost'] = "Tätä viestiä ei ole olemassa. Tarkista osoite virheiden varalta ja yritä uudelleen.";
+$l['redirect_threaddeleted'] = "Ketju poistettiin onnistuneesti.<br />Sinut ohjataan keskustelualueen etusivulle.";
+$l['redirect_postdeleted'] = "Viesti on nyt poistettu.<br />Sinut ohjataan takaisin ketjuun.";
+$l['redirect_threadclosed'] = "Et voi muokata viestejä tässä ketjussa, sillä valvoja on sulkenut tämän ketjun.";
+$l['redirect_post_moderation'] = "Ylläpito on päättänyt, että viestien muokkaus on valvonnan alaista. Siirryt pian takaisin ketjuun.";
+$l['redirect_thread_moderation'] = "Ylläpito on päättänyt, että ketjujen muokkaus on valvonnan alaista. Siirryt pian takaisin etusivulle.";
+$l['thread_deleted'] = "Ketju poistettiin";
+$l['post_deleted'] = "Viesti poistettiin";
+$l['thread_soft_deleted'] = "Soft Deleted Thread"; $l['post_soft_deleted'] = "Soft Deleted Post"; $l['thread_restored'] = "Ketju palautettu"; $l['post_restored'] = "Viesti palautettu"; $l['error_already_deleted'] = 'Valitsemasi viesti on jo poistettu.';
+?>
