710aab396ad454c73dce51b7a294426968f472a1
Joona <joona@engineer.com>
2016-11-06 21:27:16 +0200

    showteam

---
+++ finnish/showteam.lang.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * MyBB Finnish languagepack
+ * Suomentanut Joona
+ */
+
+$l['nav_showteam'] = "Vastuuhenkilöt";
+$l['forum_team'] = "Ylläpito";
+$l['moderators'] = "Valvojat";
+$l['mod_username'] = "Tunnus";
+$l['mod_forums'] = "Alueet";
+$l['mod_email'] = "Sähköposti";
+$l['mod_pm'] = "Yksityisviesti";
+$l['uname'] = "Tunnus";
+$l['email'] = "Sähköposti";
+$l['pm'] = "Yksityisviesti";
+$l['group_leaders'] = "Ryhmän vetäjä";
+$l['group_members'] = "Jäsenet";
+$l['no_members'] = "Ryhmässä ei ole jäseniä";
+$l['error_noteamstoshow'] = "Vastuuhenkilöitä ei voi näyttää, koska heitä ei ole";
+?>
